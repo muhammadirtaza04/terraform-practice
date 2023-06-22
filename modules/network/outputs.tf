6e2a3ae036d1ea0b82a99446f24b215cd6d37a4c
muhammadirtaza04 <41839811+muhammadirtaza04@users.noreply.github.com>
2023-06-22 12:09:30 +0200

    Update outputs.tf

--- modules/network/outputs.tf
+++ modules/network/outputs.tf
@@ -7,3 +7,11 @@ output "vpc_private_subnets" {
 output "vpc_public_subnets" {    
  value = { for subnet in aws_subnet.aws_public-subnets : subnet.id => subnet.cidr_block }
 }
+
+output "private_subnets_id" {   
+ value = [for subnet in aws_subnet.aws_private-subnets : subnet.id  ]
+}
+
+output "public_subnets_id" {   
+ value = [for subnet in aws_subnet.aws_public-subnets : subnet.id  ]
+}
