84b60c2155102da0776ac8e1098da1cd683ca645
muhammadirtaza04 <41839811+muhammadirtaza04@users.noreply.github.com>
2023-06-22 12:13:03 +0200

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,11 @@
-output "created_resources" {
+output "network_resources" {
   value = [   
    module.vpc.vpc_private_subnets,
    module.vpc.vpc_public_subnets,
+   module.vpc.vpc_id,
   ]
+}
+
+output "bucket_name" {
+  value = module.s3-bucket.bucket_name
 }
